b53c75f4adcecf7447133ade3a6176d641869509
Ludwig Fuhr <ludwig.fuhr@yahoo.de>
2017-09-21 20:02:53 +0200

    Non Java

---
+++ app/src/main/java/de/lfuhr/nfcdoorapp/NavigationActivity.java
@@ -0,0 +1,11 @@
+package de.lfuhr.nfcdoorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.BottomNavigationView;
+
+/**
+ * Created by ludwig on 21.09.17.
+ */
+
+public abstract class NavigationActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemReselectedListener {
+}
